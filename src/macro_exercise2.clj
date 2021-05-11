248e3a3560ec84411ca242d586f44d3ac77ec4ff
tlonist.sang <tlonist.sang@gmail.com>
2021-05-11 11:36:49 +0900

    macro-2

--- src/macro_exercise2.clj
+++ src/macro_exercise2.clj
@@ -1,1 +1,17 @@
 (ns macro_exercise2)
+
+(defmacro inspect-caller-locals []
+  (let [temp (->> (keys &env)
+                  (mapv (fn [k] `'~k)))]
+     (clojure.pprint/pprint &env)))
+
+(let [foo "bar" baz "quux"]
+  (inspect-caller-locals))
+
+(def temp1 100)
+(->> (keys {"foo" "bar" "hello" "world"})
+     (map (fn [k] [`'~k k]))
+     (into {}))
+
+(let [foo "bar"]
+  (list 'quote "bar"))
