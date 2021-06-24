d151f0d07e1a66353c107e1b869b6b616fa0d2fc
Minkyu-Jeon <minkyujeon9@gmail.com>
2021-06-24 10:19:32 +0900

    AoC 2018 day 3 refactoring

--- src/aoc2018_3.clj
+++ src/aoc2018_3.clj
@@ -2,65 +2,62 @@
   (:require [clojure.string :as str])
   (:require [clojure.core]))
 
+(defn parse-line
+  "Example:
+   (parse-line \"#1 @ 704,926: 5x4\")
+   => {:id 1 :x 704 :y 926 :w 5 :h 4}"
+  [line]
+  (->> (str/split line #"[^0-9]")
+       (filter (complement str/blank?))
+       (map #(. Integer parseInt %))
+       (zipmap [:id :x :y :w :h])))
 
-(defn build-fabric [size]
-  (->> (range 0 size)
-       (mapcat (fn [n] (->> (range 0 size)
-                            (map (fn [m] [[m n] 0]))
-                            vec)))
-       vec
-       (into (sorted-map))))
-
-(defn parse-line [line]
-  (into {} (->>
-            (-> line
-                (str/split #"[^0-9]"))
-            (filter (complement str/blank?))
-            (map #(. Integer parseInt %))
-            (map vector [:id :x :y :w :h]))))
+(def claims (->> "input/day3_input.txt"
+                 (slurp)
+                 (str/split-lines)
+                 (map parse-line)))
 
 (defn make-coords [line]
-  (let [id (:id line)
-        x (+ (:x line) 1)
-        y (+ (:y line) 1)
-        width (:w line)
-        height (:h line)]
+  (let [{:keys [x y w h]} line
+        x (inc x)
+        y (inc y)]
     (->>
-     (range 0 height)
-     (mapcat (fn [i] (->> (range 0 width)
+     (range 0 h)
+     (mapcat (fn [i] (->> (range 0 w)
                           (map (fn [j] [(+ x j) (+ y i)]))))))))
 
 (defn update-fabric [fabric claim]
-  (reduce (fn [acc coord] (update acc coord inc))
+  (reduce (fn [acc coord] (if (get acc coord)
+                            (update acc coord + 1)
+                            (assoc acc coord 1)))
           fabric
           (make-coords claim)))
 
-(def claims (->> "input/day3_input.txt"
-               (slurp)
-               (str/split-lines)
-               (map parse-line)))
+(defn validate-not-overlaped
+  "fabric(전체 맵), claim정보를 받아 해당 영역에 겹치는 부분이 없는지 판단하여
+   없으면 line, 있으면 false를 리턴"
+  [fabric claim]
+  (let [area (->> (make-coords claim)
+                  (map #(= (fabric %) 1)))]
+    (if (every? true? area)
+      claim
+      false)))
 
-(def fabric (build-fabric 1001))
 
-(defn solve1 [fabric claims]
-  (reduce (fn [acc claim] (update-fabric acc claim))
-          fabric
-          claims))
+(def updated-fabric (->> claims
+                         (reduce (fn [acc claim] (update-fabric acc claim))
+                                 {})))
 
-(def updated-fabric (solve1 fabric claims))
-
-(defn solve2 [fabric input]
+(defn find-not-overlapped-area [fabric input]
   (loop [[first & rest] input]
-    (let [claim (->> (make-coords first)
-                     (map (fn [coord] (= (get fabric coord) 1))))]
-      (if (every? true? claim)
-        first
-        (recur rest)))))
+    (if-let [claim (validate-not-overlaped fabric first)]
+      claim
+      (recur rest))))
 
 ;; Part 1
 (comment (->>
           updated-fabric
-          (filter (fn [[k v]] (> v 1)))
+          (filter (fn [[_ v]] (> v 1)))
           (count)))
 ;; Part 2
-(comment (:id (solve2 updated-fabric claims)))
+(comment (:id (find-not-overlapped-area updated-fabric claims)))
