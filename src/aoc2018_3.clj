2af51d03bedc480f146dc781932a352dc366e72a
Minkyu-Jeon <minkyujeon9@gmail.com>
2021-06-27 16:11:52 +0900

    day3 원섭님 리뷰 반영
    - 함수명 변경
    - 변수명 변경
    - 네임스페이스 as 제거

--- src/aoc2018_3.clj
+++ src/aoc2018_3.clj
@@ -1,5 +1,5 @@
 (ns aoc2018-3
-  (:require [clojure.string :as str])
+  (:require [clojure.string])
   (:require [clojure.core]))
 
 (defn parse-line
@@ -7,14 +7,14 @@
    (parse-line \"#1 @ 704,926: 5x4\")
    => {:id 1 :x 704 :y 926 :w 5 :h 4}"
   [line]
-  (->> (str/split line #"[^0-9]")
-       (filter (complement str/blank?))
+  (->> (clojure.string/split line #"[^0-9]")
+       (filter (complement clojure.string/blank?))
        (map #(. Integer parseInt %))
        (zipmap [:id :x :y :w :h])))
 
 (def claims (->> "input/day3_input.txt"
                  (slurp)
-                 (str/split-lines)
+                 (clojure.string/split-lines)
                  (map parse-line)))
 
 (defn make-coords [line]
@@ -33,7 +33,7 @@
           fabric
           (make-coords claim)))
 
-(defn validate-not-overlaped
+(defn not-overlaped?
   "fabric(전체 맵), claim정보를 받아 해당 영역에 겹치는 부분이 없는지 판단하여
    없으면 line, 있으면 false를 리턴"
   [fabric claim]
@@ -41,16 +41,16 @@
                   (map #(= (fabric %) 1)))]
     (if (every? true? area)
       claim
-      nil)))
+      false)))
 
 
 (def updated-fabric (->> claims
                          (reduce (fn [acc claim] (update-fabric acc claim))
                                  {})))
 
-(defn find-not-overlapped-area [fabric input]
-  (loop [[first & rest] input]
-    (if-let [claim (validate-not-overlaped fabric first)]
+(defn find-not-overlapped-area [fabric claims]
+  (loop [[claim & rest] claims]
+    (if (not-overlaped? fabric claim)
       claim
       (recur rest))))
 
