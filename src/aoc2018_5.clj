69aba16b3b1a46add04e913d2287748bdd974ca8
Minkyu-Jeon <minkyujeon9@gmail.com>
2021-06-28 16:05:51 +0900

    aoc 2018 day5 - part2

--- src/aoc2018_5.clj
+++ src/aoc2018_5.clj
@@ -25,8 +25,30 @@
         (recur (conj result first-item) rest))
       (conj result first-item))))
 
+(def char-vec (->> (zipmap
+                    (range
+                     (int \A)
+                     (int \Z))
+                    (range
+                     (int \a)
+                     (int \z)))
+                   (map (fn [item] (into #{} item)))))
 
+(defn remove-chars [polymer char-set]
+  (->> polymer
+       (filter (fn [item] ((complement contains?) char-set item)))))
+
+;; part1
 (comment (->> input
               (react-polymer)
               (count)))
 
+;; part2
+(comment (->> char-vec
+              (map (fn [char-set]
+                     (let [removed-chars (remove-chars input char-set)]
+                       {char-set (->> removed-chars
+                                      (react-polymer)
+                                      (count))})))
+              (apply conj)
+              (apply min-key val)))
